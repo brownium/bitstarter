b60f89356ed622f2913856b584dff9774b0af99c
Stephen Brown <brownium@gmail.com>
2013-07-11 22:26:05 +0000

    new web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  srcFile="./index.html";
+  srcString=fs.readFileSync(srcFile).toString();
+  response.send(srcString);
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
